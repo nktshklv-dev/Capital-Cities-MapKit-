f4c03410100bcf8b50a82bf2cf640b4b564ee873
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-07-12 00:08:27 +0300

    added the Outlet connection

--- Capital Cities/ViewController.swift
+++ Capital Cities/ViewController.swift
@@ -4,11 +4,13 @@
 //
 //  Created by Nikita  on 7/11/22.
 //
-
+import MapKit
 import UIKit
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
