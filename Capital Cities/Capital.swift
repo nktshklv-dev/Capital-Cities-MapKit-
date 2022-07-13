c622a6472609a65e63cdc9ded3a061e9150a9b05
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-07-13 13:29:40 +0300

    done

---
+++ Capital Cities/Capital.swift
@@ -0,0 +1,24 @@
+//
+//  Capital.swift
+//  Capital Cities
+//
+//  Created by Nikita  on 7/13/22.
+//
+import MapKit
+import UIKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String){
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+    
+ 
+    
+
+}
